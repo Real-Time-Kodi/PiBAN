cad055012797f20b68c59f54fa17265e16af82c5
Real-Time-Kodi <cody@randomosity.net>
2017-03-07 01:49:50 +0000

    We can now unlock drives from previous failed attempts

--- secure_erase.sh
+++ secure_erase.sh
@@ -5,11 +5,19 @@
 # 0: OK
 # 1: Drive should be erased manually
 # 2: ERROR(Drive is locked)
+# Note that we aren't checking for freezing because it shouldn't happen on a
+# raspi.
 erase_mode="--security-erase"
+drive=$1
 
-sec=$(hdparm -I $1 | pcregrep -M "^Security:.*(\n\t.*)*")
+# Determine if our drive supports security using HDPARM. If it does, 
+# Store it and return 0 else return 1
+readSecurity() {
+	sec=$(hdparm -I $drive | pcregrep -M "^Security:.*(\n\t.*)*")
+	return $?
+}
 
-if [ $? -eq 0 ]
+if readSecurity
 then
 	echo "Security Supported"
 else
@@ -17,6 +25,22 @@ else
 	exit 1
 fi
 echo "$sec"
+
+echo "$sec" | grep -q -P "^\t\tenabled"
+if [ $? -eq 0 ]
+then
+	echo "Security enabled. Attempting to disable."
+	if hdparm --user-master u --security-disable "password" $drive
+	then
+		echo "Disabled Successfully"
+		#Update $sec for the next group of ifs.
+		readSecurity
+	else
+		echo "Disable failed."
+		exit 1
+	fi
+fi
+
 
 echo "$sec" | grep -q -P "^\tnot\tlocked"
 if [ $? -eq 0 ]
@@ -26,12 +50,21 @@ else
 	echo "$sec" | grep -q -P "^\t\tlocked"
 	if [ $? -eq 0 ]
 	then
-		echo "Actually Locked"
-		#Todo Check if we're unlocked and try again
-		sudo hdparm --security-disable "password" $1
-		exit 2
+		echo "Weird drive(locked but security disabled)"
+		#Half-assed attempt to unlock the drive. If we're at this line
+		#and the drive is locked, there's something wrong.
+		if hdparm --user-master u --security-unlock "password" $drive
+		then
+		#once again, if we're here, we've failed to disable security, so we'll use fallback
+			exit 1
+		else
+			exit 2
+		fi
 	else
-		echo "Weird Drive"
+		#This is here because I have a weird drive that reports that it
+		#Supports secure erase, but not security. Just use a fallback in
+		#that case.
+		echo "Weird Drive(not locked or unlocked)"
 		exit 1
 	fi
 fi
@@ -43,9 +76,10 @@ then
 	erase_mode="--security-erase-enhanced"
 fi
 
-#Lock the drive(To enable security)
-hdparm --user-master u --security-set-pass "password" "$1"
+
+#Enable security
+hdparm --user-master u --security-set-pass "password" $drive
 #Run our secure-erase
-hdparm --user-master u $erase_mode "password" "$1"
+hdparm --user-master u $erase_mode "password" $drive
 
 exit $?
